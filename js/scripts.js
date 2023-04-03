217838a8cd658ed636165915c82563a6c88c8638
Chris Terrazas <chris.terrazas@gmail.com>
2023-04-03 15:25:08 -0400

    generates msgs, does all the things

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,39 @@
 const fortuneBtn = document.getElementById("fortune-btn")
+
+const options = {
+    time: [
+        'Today',
+        'Tomorrow',
+        'This week',
+        'This month',
+        'This year'
+    ],
+    event: [
+        'you will be on a boat',
+        'you will see a dog',
+        'you will be at a carnival',
+        'you will be minding your own business',
+        'you will be going to the store'
+    ],
+    resolution: [
+        'your head will explode',
+        'you will be accosted by a bowl of chili',
+        'you will follow the money and stop being one of the sheeple',
+        'you will get run over by stampeding hari krishnas',
+        'you will win a lot of money and the rights to the island of New Guinea',
+    ],
+}
+
+const displayFortune = document.getElementById("fortune")
+
 fortuneBtn.addEventListener('click', () => {
-    console.log("clicked")
+    const time = getMessage('time')
+    const event = getMessage('event')
+    const resolution = getMessage('resolution')
+    displayFortune.innerHTML = `${time}, ${event}, and ${resolution}`
 })
+
+const getMessage = section => {
+    return options[section][Math.floor(Math.random() * options[section].length)]
+}
+
